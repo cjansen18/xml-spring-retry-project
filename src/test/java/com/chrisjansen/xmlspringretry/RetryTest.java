1f45cc0242fb35c0050bafb07f59932d5ffe9989
cjansen18 <cjansen18@gmail.com>
2015-01-27 11:47:04 -0500

    Added a few methods to test behavior when returning null

--- src/test/java/com/chrisjansen/xmlspringretry/RetryTest.java
+++ src/test/java/com/chrisjansen/xmlspringretry/RetryTest.java
@@ -60,31 +60,59 @@ public class RetryTest {
     }
 
     /**
-     * This method should not use the retry logic as the exception is not configured for inclusion
+     * This method should not use the retry logic as the exception is not configured for inclusion.
+     *
+     * Observe that the method is only executed once.
      */
-    @Test(expected = IOException.class)
-    public void invokeWithoutRetrySupport() {
-        LOG.info("Running WITH RETRY");
+    @Test(expected = java.io.IOException.class)
+    public void invokeWithoutRetrySupport() throws Exception {
+        LOG.info("Running WITHOUT RETRY");
+
+        retryTemplate.execute(new RetryCallback<Boolean>() {
+            public Boolean doWithRetry(RetryContext retryContext) throws Exception {
+                int hardcodedNumber = 2;
+                LOG.info("Try number: " + retryContext.getRetryCount() + " with hard coded number: " + hardcodedNumber);
+                if (hardcodedNumber == 4) {
+                    //return Double.valueOf("We have a four in the try number: "+retryContext.getRetryCount());
+                    LOG.debug("We have a four in the try number:" + retryContext.getRetryCount());
+                    return Boolean.TRUE;
+                } else {
+                    //This exception is configured to trigger the retry logic!
+                    throw new java.io.IOException("No fours for you :(");
+                }
+            }
+        });
+    }
+
+    /**
+     * This method should use the retry logic.
+     *
+     * Crafted this method up to see retry template's behavior when returning null.
+     */
+    @Test
+    public void invokeWithoutRetrySupportAndWithoutTemplateReturns() {
+        LOG.info("Running WITHOUT RETRY AND NULL TEMPLATE RETURN");
         try {
-            retryTemplate.execute(new RetryCallback<Boolean>() {
-                public Boolean doWithRetry( RetryContext retryContext ) throws Exception {
+            retryTemplate.execute(new RetryCallback() {
+                public Object doWithRetry( RetryContext retryContext ) throws Exception {
                     Random r = new Random();
-                    int randomNumber = r.nextInt(10);
-                    LOG.info("Try number: "+retryContext.getRetryCount()+" with generated random number: "+randomNumber);
+                    int randomNumber = r.nextInt(5);
+                    LOG.info("Try number: "+retryContext.getRetryCount()+" with hard coded number: "+randomNumber);
                     if(randomNumber==4){
                         //return Double.valueOf("We have a four in the try number: "+retryContext.getRetryCount());
-                        LOG.debug("We have a four in the try number:"+retryContext.getRetryCount());
-                        return Boolean.TRUE;
+                        LOG.debug("We have a four in the attempt number:"+retryContext.getRetryCount());
+                        return null;
                     }
                     else{
                         //This exception is configured to trigger the retry logic!
-                        throw new IOException("No fours for you :(");
+                        throw new org.springframework.jms.UncategorizedJmsException("No fours for you :(");
                     }
                 }
             });
         } catch ( Exception e ) {
             e.printStackTrace();
         }
+        LOG.info("Ending Run WITHOUT RETRY AND NULL TEMPLATE RETURN");
     }
 }
 
